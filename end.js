6c8bce9650093f1f8df9254ae53f7c8df06b206c
ghost4radio <101571094+ghost4radio@users.noreply.github.com>
2023-09-03 19:28:37 +0200

    Update end.js

--- end.js
+++ end.js
@@ -2,7 +2,7 @@ let button = document.getElementById("exit");
 var audio = document.getElementById("play");
 
 button.onclick = function() {
-    window.close();
+    window.location.href="about:blank";
 }
 
 
